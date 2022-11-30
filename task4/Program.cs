777ef2f15b5b302170d4f4e824afc761b22aca5b
Aleksandr Slesarev <slessarevalex@ya.ru>
2022-11-30 10:15:14 +0300

    Решил четвертую задачу

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Clear();
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+if (N<0) //проверка на отрицательное значение
+    {
+        int i=N;
+        while (i <=1)
+            {
+                if (i % 2 == 0) Console.Write($"{i}, "); //проверка четности. Если да - то выводим
+                i++; 
+            }
+    }
+else
+    {
+        int i=2;
+        while (i<=N)
+        {
+            Console.Write($"{i}, ");
+            i+=2;
+        }
+    }

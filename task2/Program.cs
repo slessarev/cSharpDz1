622542ed63bc1f873eedc96ed6c81f8dbc6029ab
Aleksandr Slesarev <slessarevalex@ya.ru>
2022-11-30 09:31:35 +0300

    Решил вторую задачу

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// SЗадача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Clear();
+Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int num3 = int.Parse(Console.ReadLine());
+int max = num1;
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
+Console.WriteLine($"Максимальное число: {max}");

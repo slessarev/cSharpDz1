5abd447d4833df7d10f17523f0a0bf80da34d6fb
Aleksandr Slesarev <slessarevalex@ya.ru>
2022-11-30 09:44:31 +0300

    Решил третью задачу

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+Console.Clear();
+Console.Write("Введите число для проверки четности: ");
+int num = int.Parse(Console.ReadLine());
+if (num % 2 == 0) Console.Write($"Число {num} четное");
+else Console.WriteLine($"Число {num} не четное");

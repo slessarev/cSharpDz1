6c6322ea7ab0516522f7adf0a85ae77cf413e66b
Aleksandr Slesarev <slessarevalex@ya.ru>
2022-11-30 11:52:56 +0300

    Причесал код

--- task1/Program.cs
+++ task1/Program.cs
@@ -4,20 +4,9 @@ Console.WriteLine("Введите первое число");
 int num1 = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите второе число");
 int num2 = int.Parse(Console.ReadLine());
-if (num1>num2)
-{
-    Console.Write($"Число {num1} больше чем {num2}");
-  }
+if (num1>num2) Console.Write($"Число {num1} больше чем {num2}");
 else
     {
-        if (num1 < num2)
-        {
-         Console.Write($"Число {num2} больше чем {num1}"); 
-        }
-        else
-        {
-            Console.Write($"Число {num2} равно  числу {num1}");
-        }
+        if (num1 < num2) Console.Write($"Число {num2} больше чем {num1}"); 
+        else Console.Write($"Число {num2} равно  числу {num1}")            
     }
-
-
